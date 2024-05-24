a2e4111333e4597723b514fdd4bb0c8fd3b030a8
Nathan Sobo <nathan@zed.dev>
2024-05-23 20:33:54 -0600

    WIP
    I'm experimenting with manual invocation of / commands via
    confirmation of the autocomplete. I've commented out the continuous
    reparsing of / commands on edit and I'm successfully logging when
    a file path is confirmed. The next step is to invoke file insertion
    in the confirm callback.

--- crates/assistant/src/assistant_panel.rs
+++ crates/assistant/src/assistant_panel.rs
@@ -1706,7 +1706,7 @@ impl Conversation {
         if *event == language::Event::Edited {
             self.count_remaining_tokens(cx);
             self.reparse_edit_suggestions(cx);
-            self.reparse_slash_command_calls(cx);
+            // self.reparse_slash_command_calls(cx);
             cx.emit(ConversationEvent::MessagesEdited);
         }
     }
--- crates/editor/src/editor.rs
+++ crates/editor/src/editor.rs
@@ -3834,6 +3834,7 @@ impl Editor {
 
         let snippet;
         let text;
+
         if completion.is_snippet() {
             snippet = Some(Snippet::parse(&completion.new_text).log_err()?);
             text = snippet.as_ref().unwrap().text.clone();
@@ -3926,6 +3927,10 @@ impl Editor {
 
             this.refresh_inline_completion(true, cx);
         });
+
+        if let Some(confirm) = completion.confirm.as_ref() {
+            (confirm)(cx);
+        }
 
         let provider = self.completion_provider.as_ref()?;
         let apply_edits = provider.apply_additional_edits_for_completion(
